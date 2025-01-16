1d1d59d09b82357b3ff829a8bb41bc4ad12b9784
Liza Garg <lizagarg5@gmail.com>
2025-01-17 00:31:21 +0530

    day 24

---
+++ sliding window/fruitsIntoBasket.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int l=0,r=0;
+        int maxi=0;
+        unordered_map<int,int>mp;
+        int n= fruits.size();
+
+        while(r<n) {
+            mp[fruits[r]]++;
+            if(mp.size()>2) {
+                while(mp.size()>2) {
+                    mp[fruits[l]]--;
+                    if(mp[fruits[l]]==0) mp.erase(fruits[l]);
+                    l++;
+                }
+            }
+            maxi=max(maxi,r-l+1);
+            r++;
+        }
+        return maxi;
+    }
+};
